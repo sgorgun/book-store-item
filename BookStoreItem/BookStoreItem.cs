cb76ea5e7dc62b320ad18cd0b1d0f63664673613
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-12-31 12:27:55 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ BookStoreItem/BookStoreItem.cs
@@ -0,0 +1,143 @@
+using System.Globalization;
+
+[assembly: CLSCompliant(true)]
+
+namespace BookStoreItem
+{
+    /// <summary>
+    /// Represents an item in a book store.
+    /// </summary>
+    // TODO Declare a class.
+    {
+        // TODO Add fields.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookStoreItem"/> class with the specified <paramref name="authorName"/>, <paramref name="title"/>, <paramref name="publisher"/> and <paramref name="isbn"/>.
+        /// </summary>
+        /// <param name="authorName">A book author's name.</param>
+        /// <param name="title">A book title.</param>
+        /// <param name="publisher">A book publisher.</param>
+        /// <param name="isbn">A book ISBN.</param>
+        // TODO Add a constructor.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookStoreItem"/> class with the specified <paramref name="authorName"/>, <paramref name="isni"/>, <paramref name="title"/>, <paramref name="publisher"/> and <paramref name="isbn"/>.
+        /// </summary>
+        /// <param name="authorName">A book author's name.</param>
+        /// <param name="isni">A book author's ISNI.</param>
+        /// <param name="title">A book title.</param>
+        /// <param name="publisher">A book publisher.</param>
+        /// <param name="isbn">A book ISBN.</param>
+        // TODO Add a constructor.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookStoreItem"/> class with the specified <paramref name="authorName"/>, <paramref name="title"/>, <paramref name="publisher"/> and <paramref name="isbn"/>, <paramref name="published"/>, <paramref name="bookBinding"/>, <paramref name="price"/>, <paramref name="currency"/> and <paramref name="amount"/>.
+        /// </summary>
+        /// <param name="authorName">A book author's name.</param>
+        /// <param name="title">A book title.</param>
+        /// <param name="publisher">A book publisher.</param>
+        /// <param name="isbn">A book ISBN.</param>
+        /// <param name="published">A book publishing date.</param>
+        /// <param name="bookBinding">A book binding type.</param>
+        /// <param name="price">An amount of money that a book costs.</param>
+        /// <param name="currency">A price currency.</param>
+        /// <param name="amount">An amount of books in the store's stock.</param>
+        // TODO Add a constructor.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookStoreItem"/> class with the specified <paramref name="authorName"/>, <paramref name="isni"/>, <paramref name="title"/>, <paramref name="publisher"/> and <paramref name="isbn"/>, <paramref name="published"/>, <paramref name="bookBinding"/>, <paramref name="price"/>, <paramref name="currency"/> and <paramref name="amount"/>.
+        /// </summary>
+        /// <param name="authorName">A book author's name.</param>
+        /// <param name="isni">A book author's ISNI.</param>
+        /// <param name="title">A book title.</param>
+        /// <param name="publisher">A book publisher.</param>
+        /// <param name="isbn">A book ISBN.</param>
+        /// <param name="published">A book publishing date.</param>
+        /// <param name="bookBinding">A book binding type.</param>
+        /// <param name="price">An amount of money that a book costs.</param>
+        /// <param name="currency">A price currency.</param>
+        /// <param name="amount">An amount of books in the store's stock.</param>
+        // TODO Add a constructor.
+
+        /// <summary>
+        /// Gets a book author's name.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets an International Standard Name Identifier (ISNI) that uniquely identifies a book author.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets a value indicating whether an author has an International Standard Name Identifier (ISNI).
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets a book title.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets a book publisher.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets a book International Standard Book Number (ISBN).
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets or sets a book publishing date.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets or sets a book binding type.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets or sets an amount of money that a book costs.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets or sets a price currency.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets or sets an amount of books in the store's stock.
+        /// </summary>
+        // TODO Add a property.
+
+        /// <summary>
+        /// Gets a <see cref="Uri"/> to the contributor's page at the isni.org website.
+        /// </summary>
+        /// <returns>A <see cref="Uri"/> to the contributor's page at the isni.org website.</returns>
+        // TODO Add an instance method.
+
+        /// <summary>
+        /// Gets an <see cref="Uri"/> to the publication page on the isbnsearch.org website.
+        /// </summary>
+        /// <returns>an <see cref="Uri"/> to the publication page on the isbnsearch.org website.</returns>
+        // TODO Add an instance method.
+
+        /// <summary>
+        /// Returns the string that represents a current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        // TODO Add an instance method.
+
+        // TODO Add a static method.
+
+        // TODO Add a static method.
+
+        // TODO Add a static method.
+
+        // TODO Add a static method.
+    }
+}
